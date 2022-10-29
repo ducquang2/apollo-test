d90213417b717d868af23b8f04931c1fe5d93b9b
ducquang2 <nducquangg@gmail.com>
2022-10-29 11:47:22 +0700

    add Ships

--- src/routes/Ships.tsx
+++ src/routes/Ships.tsx
@@ -1,9 +1,10 @@
-import { gql } from "@apollo/client"
+import { gql, useQuery } from "@apollo/client"
 
 export default function Ships() {
   return (
     <div>
       <h2>Ships</h2>
+      <DisplayShips />
     </div>
   )
 }
@@ -11,6 +12,7 @@ export default function Ships() {
 const GET_SHIPS = gql`
   query GetShips {
     launchesPast(limit: 10) {
+      mission_name
       ships {
         name
         home_port
@@ -19,3 +21,30 @@ const GET_SHIPS = gql`
     }
   }
 `
+
+function DisplayShips() {
+  const { loading, error, data } = useQuery(GET_SHIPS);
+
+  if (loading) return <p>Loading ...</p>
+  if (error) return <p>Error: ${error.message}</p>
+
+  return (
+    <div>
+      <ul>
+        {data.launchesPast.map((item: any, index: any) =>
+          <li key={index}>
+            <p><b>Misson name:</b> {item.mission_name}</p>
+            <p><b>Ships:</b></p>
+            <ul>{item.ships.map((ship: any, idx: any) =>
+              <li key={idx}>
+                <p>{ship.name}</p>
+                <p>{ship.home_port}</p>
+                <img style={{width: "500px"}} src={ship.image} />
+              </li>
+            )}</ul>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
